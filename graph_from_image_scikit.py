e8749cfdf6eebbb811964279022436dc60417550
sollall <komugikoakasya@gmail.com>
2023-02-13 09:32:15 +0900

    del unnesessary code

--- graph_from_image_scikit.py
+++ graph_from_image_scikit.py
@@ -40,38 +40,6 @@ if False:
 fig, ax = plt.subplots()
 cmap = plt.cm.RdYlGn
 
-# プロット点をマウスオーバーでラベル表示
-if False: 
-    sc = plt.scatter(x_embedded[:, 0], x_embedded[:, 1], c=mnist_int/10.0, cmap=cmap, s=3)
-    annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
-                    bbox=dict(boxstyle="round", fc="w"),
-                    arrowprops=dict(arrowstyle="->"))
-    annot.set_visible(False)
-    
-    def update_annot(ind):
-        i = ind["ind"][0]
-        pos = sc.get_offsets()[i]
-        annot.xy = pos
-        text = mnist_label[i]
-        annot.set_text(text)
-        annot.get_bbox_patch().set_facecolor(cmap(int(text)/10))
-
-    def hover(event):
-        vis = annot.get_visible()
-        if event.inaxes == ax:
-            cont, ind = sc.contains(event)
-            if cont:
-                #update_annot(ind)
-                annot.set_visible(True)
-                fig.canvas.draw_idle()
-            else:
-                if vis:
-                   annot.set_visible(False)
-                   fig.canvas.draw_idle()
-
-    fig.canvas.mpl_connect("motion_notify_event", hover)
-
-    plt.show()
 
 # プロット点をマウスオーバーで画像表示
 if True:
